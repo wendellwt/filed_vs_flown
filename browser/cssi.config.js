3934f73dba0f6672a54cfd4d05e1953e93fcc6e4
U-CSSI\WTurner <WTurner@ASDI-DB.cssiinc.com>
2021-01-16 17:20:20 -0500

    fvf index page displays

--- browser/cssi.config.js
+++ browser/cssi.config.js
@@ -4,9 +4,10 @@ const path = require("path");
 module.exports = {
 
     // Note: the number here is the one assigned by the 1st 'rconnect deploy'
-    publicPath: "/",
+    publicPath: "/fvf/",
 
-    outputDir: "../service/code/",
+    // custom: asdex/service is the "official" server dir
+    outputDir: "../../asdex/service/fvf_code/",
 
     "transpileDependencies": [
         "buefy"
