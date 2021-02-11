aac1d4e8bed09b2312ac43e1f0c463ee505088ea
Wendell CTR Turner <Wendell.CTR.Turner@faa.gov>
2021-02-10 19:03:11 -0500

    possible deploy updates

--- rs_deploy/app.py
+++ rs_deploy/app.py
@@ -14,6 +14,7 @@ sys.path.append('copied')  # stupid git/sharing  work-around
 import get_paths
 import fvf_by_artcc
 import everything    # CATION: each of these makes a new PosgGreSQL connection
+import get_feb       # CATION: each of these makes a new PosgGreSQL connection
 import web_logging
 
 # --------------------------------------------------------------------
@@ -151,6 +152,29 @@ def do_everything():
     #lgr.info("+++++")
 
     return every_js
+
+# --------------------------------------------------------------------
+
+import datetime
+
+@app.route('/get_feb', methods=['GET'])
+
+def do_feb():
+
+    lgr.info("get_feb - in")
+
+    #airport = request.args['apt']
+    #center = request.args['ctr']
+    gdate = datetime.datetime.strptime( request.args['date'], '%Y-%m-%d')
+
+    feb_js = get_feb.get_postg_data_from_asdidb(lgr, gdate)
+
+    lgr.info("get_feb - out")
+    #lgr.info("+++++")
+    #lgr.info(feb_js)
+    #lgr.info("+++++")
+
+    return feb_js
 
 #######################################################################
 #                            main                                     #
