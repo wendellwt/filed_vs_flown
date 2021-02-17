b63ebfce429ac5e8f63f4a410650460fca27652c
Wendell CTR Turner <Wendell.CTR.Turner@faa.gov>
2021-02-17 10:52:03 -0500

    cleanup names

--- browser/src/components/Tabs.vue
+++ browser/src/components/Tabs.vue
@@ -13,10 +13,10 @@
 
             <div class="columns">
               <div class="column">
-                <app-ol></app-ol>
+                <OL></OL>
               </div>
               <div class="column is-one-quarter">
-                <data-pos></data-pos>
+                <DataPos></DataPos>
               </div>
             </div>
          </b-tab-item>
@@ -34,7 +34,7 @@
          <b-tab-item label="Table"><mytable /></b-tab-item> -->
 
          <!-- ============= details tab ============== -->
-         <b-tab-item label="Details"><mydetails /></b-tab-item>
+         <b-tab-item label="Details"><Details /></b-tab-item>
 
          <!-- ============= about tab ============== -->
          <b-tab-item label="About"><About /></b-tab-item>
@@ -46,30 +46,33 @@
 
 <script>
 
+import Model      from "./Model.vue";
+import FiledEntry from "./FiledEntry.vue";
 import OL         from './OL.vue';
 import DataPos    from "./DataPos.vue";
-import Charts     from "./Charts.vue";
-//import Stacked    from "./Stacked.vue";
-import FiledEntry from "./FiledEntry.vue";
 import Details    from "./Details.vue";
 import About      from "./About.vue";
-import Circular   from "./Circular.vue";
+//import Charts     from "./Charts.vue";
+//import Stacked    from "./Stacked.vue";
+// import Circular   from "./Circular.vue";
 //import Table   from "./Table.vue";
-import Model      from "./Model.vue";
 
 export default {
   name: 'Tabs',
     components: {
       Model,
-      'app-ol'   : OL,
-      'data-pos' : DataPos,
-      // 'mytable'  : Table,    // duh! 'table' is bad choice for a local html component!
-      'mydetails': Details,
-      Charts,
-      //Stacked,
       FiledEntry,
-      Circular,
+      OL,
+      DataPos,
+      Details,
       About
+
+        // old, experimental:
+      //Charts,
+      //Stacked,
+      // Circular,
+      // 'mytable'  : Table,    // duh! 'table' is bad choice for a local html component!
+
   },
   props: {
     msg: String
