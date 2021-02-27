645d816fe5d7b071cc22d9d78a05247a53c60f56
Wendell Turner <skyking@ilikecarrots.com>
2021-02-27 18:54:25 -0500

    separated out funcs using shapes

---
+++ etl/fcrop.py
@@ -0,0 +1,379 @@
+
+# EXPERIMENT: collect all shape cropping routines into one file
+
+import pandas as pd
+import shapefile
+from shapely import wkb
+import fpostg   # Q: does this make MULTIPLE CONNECTIONS (for each import?)
+from shapely.geometry import shape, Point, LineString, Polygon
+
+# -----------------------  read shapefile of artcc and tracon
+
+#           -     within                 ;  upto
+# parent    - inters of center minus trc ; diff with center
+# any tier1 - inters of that center      ; diff with U of cntr + all tier 1
+# any tier2 - inters of that center      ; diff with U of cntr + all tier 1 + 2
+
+# full      - (*)difference with tracon  ; na
+
+# ---- globals:
+
+within_shp = None
+tracon_shp = None
+upto_shp   = None
+
+def read_masking_shapes(ctr, tier, tracon_shapefile, all_artccs, verbose=False):
+    global within_shp, tracon_shp, upto_shp
+
+    # ---- There's always a tracon involved
+
+    # ---- 1a. read shapefile using with PyShp
+
+    tracon_esri = shapefile.Reader(tracon_shapefile)
+    tracon_feature = tracon_esri.shapeRecords()[0]
+    tracon_geom = tracon_feature.shape.__geo_interface__
+    tracon_shp = shape(tracon_geom)
+
+    if ctr == 'ZZZ':
+        within_shp = None   # just to make sure they aren't used...
+        upto_shp   = None   # just to make sure they aren't used...
+        return
+
+    # ---- 1. within shape is center minus tracon (always?)
+
+    ctr_wkb = fpostg.get_shape_of_ctr(ctr, verbose)
+
+    center_shape = wkb.loads(ctr_wkb[0][0], hex=True)
+
+    # and have _shapely_ take the difference:
+    # if center is parent, then remove tracon,
+    # if center is not parent, then this shouldn't matter (right?)
+
+    within_shp = center_shape.difference(tracon_shp)
+
+    # ---- 2. get upto shape
+
+    # 2a. if parent, then shape is just the parent center itself
+
+    if tier == 'parent':
+
+        upto_shp = center_shape
+
+    if tier == '1sttier':
+
+        upto_shp = Polygon()
+
+        for c in all_artccs:
+
+            if c[1] == 'parent' or c[1] == '1sttier':
+
+                ctr_wkb = fpostg.get_shape_of_ctr(c[0], verbose)
+                ctr_shape = wkb.loads(ctr_wkb[0][0], hex=True)
+
+                upto_shp = upto_shp.union(ctr_shape)
+
+    if tier == '2ndtier':
+
+        upto_shp = Polygon()
+
+        for c in all_artccs:
+
+            # pretty much any currently listed...
+            if c[1] == 'parent' or c[1] == '1sttier' or c[1] == '2ndtier':
+
+                ctr_wkb = fpostg.get_shape_of_ctr(c[0], verbose)
+                ctr_shape = wkb.loads(ctr_wkb[0][0], hex=True)
+
+                upto_shp = upto_shp.union(ctr_shape)
+
+# =========================================================================
+
+# ==== ==== ==== each path INTERSECTED with artcc shape
+
+# NOTE: if artcc is real, these are the WITHIN,
+#       if artcc is zzz, these are the FULL (but still without the tracon)
+
+def get_all_withins(ctr_df, ctr):
+
+    for (wp, p) in ( ('scheduled_within_path'  , 'scheduled_path'   ),
+                     ('first_filed_within_path', 'first_filed_path' ),
+                     ('last_filed_within_path' , 'last_filed_path'  ),
+                     ('flown_within_path'      , 'flown_path'       ) ):
+
+        ctr_df[wp] = ctr_df[p].apply(lambda p: do_w_intersect(p, ctr))
+
+    for (wd, wp) in ( ('scheduled_within_dist'  , 'scheduled_within_path'  ),
+                      ('first_filed_within_dist', 'first_filed_within_path'),
+                      ('last_filed_within_dist' , 'last_filed_within_path' ),
+                      ('flown_within_dist'      , 'flown_within_path'      ) ):
+
+        ctr_df[wd] = ctr_df[wp].apply(lambda p: gc_length(p))
+
+    return(ctr_df)
+
+# =========================================================================
+
+# ==== ==== ==== (cp wants these) each path DIFFERENCE with tracon
+
+def get_all_uptos(ctr_df):
+
+    for (up, p) in ( ('scheduled_upto_path'  , 'scheduled_path'   ),
+                     ('first_filed_upto_path', 'first_filed_path' ),
+                     ('last_filed_upto_path' , 'last_filed_path'  ),
+                     ('flown_upto_path'      , 'flown_path'       ) ):
+
+        ctr_df[up] = ctr_df[p].apply(lambda p: do_u_difference(p))
+
+    for (ud, up) in ( ('scheduled_upto_dist'  , 'scheduled_upto_path'  ),
+                      ('first_filed_upto_dist', 'first_filed_upto_path'),
+                      ('last_filed_upto_dist' , 'last_filed_upto_path' ),
+                      ('flown_upto_dist'      , 'flown_upto_path'      ) ):
+
+        ctr_df[ud] = ctr_df[up].apply(lambda p: gc_length(p))
+
+    return(ctr_df)
+
+# =========================================================================
+
+def fake_all_uptos(ctr_df):
+
+    ctr_df['scheduled_upto_path'  ] = LineString()
+    ctr_df['first_filed_upto_path'] = LineString()
+    ctr_df['last_filed_upto_path' ] = LineString()
+    ctr_df['flown_upto_path'      ] = LineString()
+
+    ctr_df['scheduled_upto_dist'  ] = 0
+    ctr_df['first_filed_upto_dist'] = 0
+    ctr_df['last_filed_upto_dist' ] = 0
+    ctr_df['flown_upto_dist'      ] = 0
+
+    return(ctr_df)
+
+# ========================================================================
+
+# DUH!!! this gets the ENTIRE DISTANCE OUTSIDE of the artcc!!!
+
+def do_u_difference(path_ls):
+
+    int_path = path_ls.difference(upto_shp)
+    return int_path
+
+# ++++++++++++++++++++++++++++++++++++++++++++
+
+def do_f_difference(path_ls):
+
+    int_path = path_ls.difference(tracon_shp)
+    return int_path
+
+# ++++++++++++++++++++++++++++++++++++++++++++
+
+# mon am: special check -- if ctr == ZZZ then just take tracon diff,
+#                                        else do real intersection
+
+def do_w_intersect(path_ls, ctr):
+
+    if ctr != 'ZZZ':
+        int_path = path_ls.intersection(within_shp)
+    else:
+        int_path = path_ls.difference(tracon_shp)
+    return int_path
+
+# =========================================================================
+
+from geopy import distance
+
+# ---- ---- calc G.C. DISTANCE (length) of one linestring (or multilinestring)
+
+def gc_length(ls_path):
+
+    if ls_path.is_empty:
+        return(0)
+
+    if ls_path.geom_type=="LineString":
+
+        return(help_gc_length_one_segment(ls_path))
+
+    else:
+        if ls_path.geom_type=="MultiLineString":
+            sum=0
+            for each_path in ls_path:
+
+                sum += help_gc_length_one_segment(each_path)
+
+            return(sum)
+
+        else:
+            print("HELP: don't know how to measure distance of:", ls_path.geom_type)
+            return(0)
+
+    return(0)  # ???
+
+# sometimes there is:
+# GEOMETRYCOLLECTION (POINT (-94.05 36.75), LINESTRING (-86.64 34.13, ...
+
+# =========================================================================
+
+def help_gc_length_one_segment(ls_path):
+
+    if ls_path.is_empty:
+        return(0)
+
+    if ls_path.geom_type=="MultiLineString":
+        print("HELP: MultiLineString")
+        return(99)
+
+    lon_lat = list(ls_path.coords)
+
+    # If you pass coordinates as positional args, please make sure that the
+    # order is (latitude, longitude) or (y, x) in Cartesian terms.
+
+    # swap each pair of elements to get right order for geopy's distance()
+    lat_lon = [(item[1], item[0]) for item in lon_lat]
+
+    segments = [(lat_lon[k], lat_lon[k + 1]) for k in range(len(lat_lon)-1)]
+
+    dsegs = [distance.distance(seg[0], seg[1]).nautical for seg in segments]
+
+    return(sum(dsegs))
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+def get_at_entry_sch_paths(all_scheds_df, flw_pts_df, center_shp):
+
+    # ---- first, get all TZ within the artcc:
+
+    tz_within = flw_pts_df.loc[flw_pts_df['position'].intersects(center_shp)]
+
+    # ---- second, get the first POSIT_TIME of each FID
+
+    first_tz_within = tz_within.sort_values("POSIT_TIME") \
+                               .groupby("FID", as_index=False).last()
+
+    # ---- third, match those FIDs with the original SCHED ones
+
+    sched_with_tz_df = pd.merge(first_tz_within, all_scheds_df,
+                                on='FID', how='inner')
+
+    # ---- fourth, keep just the rows where orig_time <= first tz time
+
+    scheds_outside_df = sched_with_tz_df.loc[
+              sched_with_tz_df['ORIG_TIME'] < sched_with_tz_df['POSIT_TIME'] ]
+
+    # and get the last (largest) times of those
+
+    at_entry_df = scheds_outside_df.sort_values("ORIG_TIME")  \
+                                   .groupby("FID", as_index=False).first()
+
+    return(at_entry_df)
+
+# ==========================================================================
+
+def get_before_entry_single_artcc(ctr_df, all_scheds_df, flown_pts_df):
+
+    # get df of rows from sched_df having orig_time last one before artcc entry
+
+    at_entry_df = get_at_entry_sch_paths(all_scheds_df, flown_pts_df,
+                                         upto_shp)
+
+    # and make a linestring of those waypoints, and get dist
+
+    at_entry_df['at_entry_path'] = at_entry_df.apply( lambda row:
+                                    form_linestring(row), axis=1)
+
+    for (wp, p) in ( ('at_entry_within_path', 'at_entry_path'), ):
+
+        at_entry_df[wp] = at_entry_df[p].apply(lambda p: do_f_difference(p))
+
+    for (wd, wp) in ( ('at_entry_within_dist', 'at_entry_within_path'), ):
+
+        at_entry_df[wd] = at_entry_df[wp].apply(lambda p: gc_length(p))
+
+    both_df = pd.merge(ctr_df, at_entry_df, on='FID', how='inner')
+
+    ctr_df = both_df.drop([
+'at_entry_path',
+'scheduled_path_x',
+'scheduled_path_y',
+'first_filed_path_x',
+'first_filed_path_y',
+'last_filed_path_x',
+'last_filed_path_y',
+'ACID_x',
+'ACID_y',
+'ORIG_TIME_x',
+'ORIG_TIME_y',
+'WAYPOINTS_x',
+'WAYPOINTS_y',
+
+'DEP_TIME_y',
+'ARR_TIME_y',
+'DEPT_APRT_y',
+'ARR_APRT_y',
+'ACFT_TYPE_y',
+
+'POSIT_TIME', 'position'
+        ], axis=1)
+
+    ctr_df.rename( {
+            'DEP_TIME_x'  : 'DEP_TIME',
+            'ARR_TIME_x'  : 'ARR_TIME',
+            'DEPT_APRT_x' : 'DEPT_APRT',
+            'ARR_APRT_x'  : 'ARR_APRT',
+            'ACFT_TYPE_x' : 'ACFT_TYPE',
+        }, axis=1, inplace=True)
+
+    return(ctr_df)
+
+# ==========================================================================
+
+def make_bogus_before_entry(ctr_df):
+
+    ctr_df['at_entry_within_path'] = LineString()
+    ctr_df['at_entry_within_dist'] = 0
+
+    ctr_df['FLIGHT_INDEX'] = 0  ## HELP <<<<<<<<<<<<<<
+
+    ctr_df.drop([
+                   'ORIG_TIME',
+                   'WAYPOINTS',
+                   'scheduled_path',
+                   'first_filed_path',
+                   'last_filed_path',
+                   #  'flown_path',  HELP: needs to be here to drop later
+                   # by  fpostg?
+                         ], axis=1, inplace=True)
+
+    return(ctr_df)
+
+# ======================================================================
+
+# note: this may fail and return None, which is cleaned by the dropna()
+# ALSO, lots of duplicates, TODO: remove dups
+
+def form_linestring(row):
+
+    # ---- apparently, dep and arr apts are in waypoints
+
+    # ---- 1.  form list of all waypoints (as shapely Point()s)
+
+    try:
+        wpts = row['WAYPOINTS'].split()
+
+        pts = list( map( lambda x:
+              Point(
+                 float(x.split('/')[1])/(-60.0),
+                 float(x.split('/')[0])/  60.0
+              ), wpts ) )
+    except:
+        return(None)     # something messed up, fail
+
+    #  and put them together in a (shapely) linestring
+
+    try:
+        path_ls = LineString(pts)
+    except:
+        print("bad Linestring, len=", len(pts), row['DEPT_APRT'], row['ARR_APRT'])
+        return(None)     # bad shapely linestring ( < 2 points?)
+
+    return(path_ls)
+
--- etl/filedflown.py
+++ etl/filedflown.py
@@ -16,12 +16,13 @@ import pickle
 import argparse
 import datetime
 import geopandas as gpd
-from shapely.geometry import shape, Point, LineString
+from shapely.geometry import LineString
 import code           # <<<<<<<<<<<
 
 # our own:
 import foracle
 import fpostg
+import fcrop
 import elapsed
 
 # ####################################################################### #
@@ -63,9 +64,9 @@ from shapely.wkt import loads
 
 adapt = toml.load("adaptation.toml")
 
+# Q: Move these to fcrop.py?????
 tracon_shp_dir = "/home/data/wturner/shapefiles/"
 tracon_shapefile = tracon_shp_dir + adapt[args.airport]['tracon']
-conus_shapefile  = tracon_shp_dir + "conus.shp"
 
 # ---- make table of corners
 
@@ -125,197 +126,6 @@ def get_sched_data_from_oracle(act_date, arr_apt):
     return(arrivals_df, clean_fids)
 
 # ======================================================================
-
-# note: this may fail and return None, which is cleaned by the dropna()
-# ALSO, lots of duplicates, TODO: remove dups
-
-def form_linestring(row):
-
-    # ---- apparently, dep and arr apts are in waypoints
-
-    # ---- 1.  form list of all waypoints (as shapely Point()s)
-
-    try:
-        wpts = row['WAYPOINTS'].split()
-
-        pts = list( map( lambda x:
-              Point(
-                 float(x.split('/')[1])/(-60.0),
-                 float(x.split('/')[0])/  60.0
-              ), wpts ) )
-    except:
-        return(None)     # something messed up, fail
-
-    #  and put them together in a (shapely) linestring
-
-    try:
-        path_ls = LineString(pts)
-    except:
-        print("bad Linestring, len=", len(pts), row['DEPT_APRT'], row['ARR_APRT'])
-        return(None)     # bad shapely linestring ( < 2 points?)
-
-    return(path_ls)
-
-# ======================================================================
-
-# -----------------------  read shapefile of artcc and tracon
-
-import shapefile
-from shapely import wkb
-
-# feb20: if ctr = ZZZ, then return ??? and tracon shapefile
-# feb22: wanted for path type:
-#     -        within        ;  upto       ;   full
-#     - center minus tracon  ; center      ; tracon
-#     - intersect            ; difference  ; difference
-
-# feb25: wanted for path type:
-#           -     within                 ;  upto
-# parent    - inters of center minus trc ; diff with center
-# any tier1 - inters of that center      ; diff with U of cntr + all tier 1
-# any tier2 - inters of that center      ; diff with U of cntr + all tier 1 + 2
-
-# full      - (*)difference with tracon  ; na
-
-from shapely.geometry import Polygon
-
-def read_masking_shapes(ctr, tier):
-
-    # ---- There's always a tracon involved
-
-    # ---- 1a. read shapefile using with PyShp
-    # https://gis.stackexchange.com/questions/113799/how-to-read-a-shapefile-in-python
-
-    tracon_esri = shapefile.Reader(tracon_shapefile)
-
-    # first feature of the shapefile
-    tracon_feature = tracon_esri.shapeRecords()[0]
-
-    tracon_geom = tracon_feature.shape.__geo_interface__
-
-    # ---- 1c. conversion to Shapely geometry (with the shape function)
-
-    tracon_shape = shape(tracon_geom)
-
-    if ctr == 'ZZZ':
-        return(None, None, tracon_shape)
-
-    # ---- 1. within shape is center minus tracon (always?)
-
-    ctr_wkb = fpostg.get_shape_of_ctr(ctr, args.verbose)
-
-    center_shape = wkb.loads(ctr_wkb[0][0], hex=True)
-
-    # and have _shapely_ take the difference:
-    # if center is parent, then remove tracon,
-    # if center is not parent, then this shouldn't matter (right?)
-
-    within_shape = center_shape.difference(tracon_shape)
-
-    # ---- 2. get upto shape
-
-    # 2a. if parent, then shape is just the parent center itself
-
-    if tier == 'parent':
-
-        upto_shape = center_shape
-
-    if tier == '1sttier':
-
-        upto_shape = Polygon()
-
-        for c in artccs:
-
-            if c[1] == 'parent' or c[1] == '1sttier':
-
-                ctr_wkb = fpostg.get_shape_of_ctr(c[0], args.verbose)
-                ctr_shape = wkb.loads(ctr_wkb[0][0], hex=True)
-
-                upto_shape = upto_shape.union(ctr_shape)
-
-    if tier == '2ndtier':
-
-        upto_shape = Polygon()
-
-        for c in artccs:
-
-            # pretty much any currently listed...
-            if c[1] == 'parent' or c[1] == '1sttier' or c[1] == '2ndtier':
-
-                ctr_wkb = fpostg.get_shape_of_ctr(c[0], args.verbose)
-                ctr_shape = wkb.loads(ctr_wkb[0][0], hex=True)
-
-                upto_shape = upto_shape.union(ctr_shape)
-
-    return(within_shape, upto_shape, tracon_shape)
-
-# =========================================================================
-
-from geopy import distance
-
-# ---- ---- calc G.C. DISTANCE (length) of one linestring
-
-def gc_length(ls_path):
-
-    if ls_path.is_empty:
-        return(0)
-
-    if ls_path.geom_type=="LineString":
-
-        return(help_gc_length_one_segment(ls_path))
-
-    else:
-        if ls_path.geom_type=="MultiLineString":
-            sum=0
-            for each_path in ls_path:
-
-                sum += help_gc_length_one_segment(each_path)
-
-            return(sum)
-
-        else:
-            print("HELP: don't know how to measure distance of:", ls_path.geom_type)
-            # print(ls_path) # (see below)
-            return(0)
-            #sys.exit(1)
-
-    return(0)  # ???
-
-# sometimes there is:
-# GEOMETRYCOLLECTION (POINT (-94.05 36.75), LINESTRING (-86.64 34.13, ...
-# -----------------------------------------------------------------
-
-def help_gc_length_one_segment(ls_path):
-
-    if ls_path.is_empty:
-        return(0)
-
-    #print(ls_path)
-    #print(ls_path.geom_type)
-
-    if ls_path.geom_type=="MultiLineString":
-        print("HELP: MultiLineString")
-        return(99)
-
-    lon_lat = list(ls_path.coords)
-    # print(lon_lat)
-
-    # If you pass coordinates as positional args, please make sure that the
-    # order is (latitude, longitude) or (y, x) in Cartesian terms.
-
-    # swap each pair of elements to get right order for geopy's distance()
-    lat_lon = [(item[1], item[0]) for item in lon_lat]
-    # print(lat_lon)
-
-    segments = [(lat_lon[k], lat_lon[k + 1]) for k in range(len(lat_lon)-1)]
-    # print(segments)
-
-    dsegs = [distance.distance(seg[0], seg[1]).nautical for seg in segments]
-
-    # print(sum(dsegs))
-
-    return(sum(dsegs))
-
 # =========================================================================
 
 def get_at_entry_sch_paths(all_scheds_df, flw_pts_df, center_shp):
@@ -495,7 +305,7 @@ def make_all_scheds(sched_df):
 
     # b3. and make a linestring of it while we're here
 
-    first_scheduled_df['scheduled_path'] = first_scheduled_df.apply( lambda row: form_linestring(row), axis=1)
+    first_scheduled_df['scheduled_path'] = first_scheduled_df.apply( lambda row: fcrop.form_linestring(row), axis=1)
     first_scheduled_df.dropna(inplace=True)
 
     # +++++++++++++++++++++++ first_filed_path +++++++++++++++++++++++++++
@@ -510,7 +320,7 @@ def make_all_scheds(sched_df):
 
     # b3. and make a linestring of it while we're here
 
-    first_filed_df['first_filed_path'] = first_filed_df.apply( lambda row: form_linestring(row), axis=1)
+    first_filed_df['first_filed_path'] = first_filed_df.apply( lambda row: fcrop.form_linestring(row), axis=1)
     first_filed_df.dropna(inplace=True)
 
     # +++++++++++++++++++++++ last_filed_path +++++++++++++++++++++++++++
@@ -526,7 +336,7 @@ def make_all_scheds(sched_df):
 
     # b3. and make a linestring of it while we're here
 
-    last_b4_dep_df['last_filed_path'] = last_b4_dep_df.apply( lambda row: form_linestring(row), axis=1)
+    last_b4_dep_df['last_filed_path'] = last_b4_dep_df.apply( lambda row: fcrop.form_linestring(row), axis=1)
     last_b4_dep_df.dropna(inplace=True)
 
     # +++++++++++++++++++++++
@@ -690,202 +500,6 @@ def output_postgis(ctr_df):
 
 # =========================================================================
 
-# ==== ==== ==== each path INTERSECTED with artcc shape
-# NOTE: if artcc is real, these are the WITHIN,
-#       if artcc is zzz, these are the FULL (but still without the tracon)
-
-def get_all_withins(ctr_df):
-
-    for (wp, p) in ( ('scheduled_within_path'  , 'scheduled_path'   ),
-                     ('first_filed_within_path', 'first_filed_path' ),
-                     ('last_filed_within_path' , 'last_filed_path'  ),
-                     ('flown_within_path'      , 'flown_path'       ) ):
-
-        ctr_df[wp] = ctr_df[p].apply(lambda p: do_w_intersect(p))
-
-    for (wd, wp) in ( ('scheduled_within_dist'  , 'scheduled_within_path'  ),
-                      ('first_filed_within_dist', 'first_filed_within_path'),
-                      ('last_filed_within_dist' , 'last_filed_within_path' ),
-                      ('flown_within_dist'      , 'flown_within_path'      ) ):
-
-        ctr_df[wd] = ctr_df[wp].apply(lambda p: gc_length(p))
-
-    return(ctr_df)
-
-# =========================================================================
-
-# ==== ==== ==== (cp wants these) each path DIFFERENCE with tracon
-
-def get_all_uptos(ctr_df):
-
-    for (up, p) in ( ('scheduled_upto_path'  , 'scheduled_path'   ),
-                     ('first_filed_upto_path', 'first_filed_path' ),
-                     ('last_filed_upto_path' , 'last_filed_path'  ),
-                     ('flown_upto_path'      , 'flown_path'       ) ):
-
-        ctr_df[up] = ctr_df[p].apply(lambda p: do_u_difference(p))
-
-    for (ud, up) in ( ('scheduled_upto_dist'  , 'scheduled_upto_path'  ),
-                      ('first_filed_upto_dist', 'first_filed_upto_path'),
-                      ('last_filed_upto_dist' , 'last_filed_upto_path' ),
-                      ('flown_upto_dist'      , 'flown_upto_path'      ) ):
-
-        ctr_df[ud] = ctr_df[up].apply(lambda p: gc_length(p))
-
-    # print(ctr_df)
-    # print(ctr_df.columns)
-    # print("all upto paths")
-    #feb20  code.interact(local=locals())   # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-    return(ctr_df)
-
-# =========================================================================
-
-def fake_all_uptos(ctr_df):
-
-    ctr_df['scheduled_upto_path'  ] = LineString()
-    ctr_df['first_filed_upto_path'] = LineString()
-    ctr_df['last_filed_upto_path' ] = LineString()
-    ctr_df['flown_upto_path'      ] = LineString()
-
-    ctr_df['scheduled_upto_dist'  ] = 0
-    ctr_df['first_filed_upto_dist'] = 0
-    ctr_df['last_filed_upto_dist' ] = 0
-    ctr_df['flown_upto_dist'      ] = 0
-
-    return(ctr_df)
-
-# =========================================================================
-# mon am: special check -- if ctr == ZZZ then just take tracon diff,
-#                                        else do real intersection
-
-# thurs: remove all MultiLineString checks
-
-def do_w_intersect(path_ls):
-
-    if ctr != 'ZZZ':
-        int_path = path_ls.intersection(within_shp)
-    else:
-        int_path = path_ls.difference(tracon_shp)
-    return int_path
-
-# ++++++++++++++++++++++++++++++++++++++++++++
-
-# DUH!!! this gets the ENTIRE DISTANCE OUTSIDE of the artcc!!!
-def do_u_difference(path_ls):
-
-    int_path = path_ls.difference(upto_shp)
-    return int_path
-
-# ++++++++++++++++++++++++++++++++++++++++++++
-
-def do_f_difference(path_ls):
-
-    int_path = path_ls.difference(tracon_shp)
-    return int_path
-
-# ==========================================================================
-
-def get_before_entry_single_artcc(ctr_df):
-
-    # get df of rows from sched_df having orig_time last one before artcc entry
-
-    at_entry_df = get_at_entry_sch_paths(all_scheds_df, flown_pts_df,
-                                         upto_shp)
-
-    # print("at_entry_df")
-    # print(at_entry_df)
-    #feb20 code.interact(local=locals())   # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-    # and make a linestring of those waypoints, and get dist
-
-    at_entry_df['at_entry_path'] = at_entry_df.apply( lambda row:
-                                    form_linestring(row), axis=1)
-
-    # print("at_entry_df + linestrings")
-    # print(at_entry_df)
-    #feb 20 code.interact(local=locals())   # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-    at_entry_df['at_entry_within_path'] = at_entry_df['at_entry_path'       ].apply(lambda p: do_f_difference(p))
-    at_entry_df['at_entry_within_dist'] = at_entry_df['at_entry_within_path'].apply(lambda p: gc_length(p))
-
-    both_df = pd.merge(ctr_df, at_entry_df, on='FID', how='inner')
-
-    ctr_df = both_df.drop([
-'at_entry_path',
-'scheduled_path_x',
-'scheduled_path_y',
-'first_filed_path_x',
-'first_filed_path_y',
-'last_filed_path_x',
-'last_filed_path_y',
-'ACID_x',
-'ACID_y',
-'ORIG_TIME_x',
-'ORIG_TIME_y',
-'WAYPOINTS_x',
-'WAYPOINTS_y',
-
-'DEP_TIME_y',
-'ARR_TIME_y',
-'DEPT_APRT_y',
-'ARR_APRT_y',
-'ACFT_TYPE_y',
-
-'POSIT_TIME', 'position'
-        ], axis=1)
-
-    ctr_df.rename( {
-            'DEP_TIME_x'  : 'DEP_TIME',
-            'ARR_TIME_x'  : 'ARR_TIME',
-            'DEPT_APRT_x' : 'DEPT_APRT',
-            'ARR_APRT_x'  : 'ARR_APRT',
-            'ACFT_TYPE_x' : 'ACFT_TYPE',
-        }, axis=1, inplace=True)
-
-    # print(ctr_df)
-    # print("new ctr_df with at_entry_within != ZZZ")
-    #print(ctr_df.columns)
-
-    #for C in ctr_df.columns.tolist():
-    #    print(C, "     ", type(ctr_df[C][0]))
-
-    #feb20  code.interact(local=locals())   # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-    return(ctr_df)
-
-# ==========================================================================
-
-def make_bogus_before_entry(ctr_df):
-
-    ctr_df['at_entry_within_path'] = LineString()
-    ctr_df['at_entry_within_dist'] = 0
-
-    ctr_df['FLIGHT_INDEX'] = 0  ## HELP <<<<<<<<<<<<<<
-
-    ctr_df.drop([
-                   'ORIG_TIME',
-                   'WAYPOINTS',
-                   'scheduled_path',
-                   'first_filed_path',
-                   'last_filed_path',
-                   #  'flown_path',  HELP: needs to be here to drop later
-                   # by  fpostg
-                         ], axis=1, inplace=True)
-
-    # print(ctr_df)
-    # print("new ctr_df with at_entry_within = ZZZ")
-    #print(ctr_df.columns)
-
-    #for C in ctr_df.columns.tolist():
-    #    print(C, "     ", type(ctr_df[C][0]))
-
-    #feb20 code.interact(local=locals())   # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-    return(ctr_df)
-
-# =========================================================================
-
 def finalize_cols(ctr_df, ctr):
 
     # put this in postgis also, just to keep them consistent...
@@ -956,16 +570,14 @@ for ctr, tier in artccs:
 
     # ==== b. get ARTCC polygon of interest
 
-    # center_minus_tracon_shp, center_shp, tracon_shp = read_masking_shapes(ctr)
-
-    within_shp, upto_shp, tracon_shp = read_masking_shapes(ctr, tier)
+    fcrop.read_masking_shapes(ctr, tier, tracon_shapefile, artccs, args.verbose)
 
     # ==== ==== ====
 
     #  and get an intersection column
     # NOTE: is is not a GeoPandas df, but a Pandas df with geometry columns
 
-    ctr_df = get_all_withins(ctr_df)
+    ctr_df = fcrop.get_all_withins(ctr_df, ctr)
 
     #if ctr == 'ZMP' or ctr == 'ZAU':
 
@@ -979,20 +591,20 @@ for ctr, tier in artccs:
     # ==== ==== ==== (cp wants these) each path DIFFERENCE with tracon
 
     if ctr != 'ZZZ':
-        ctr_df = get_all_uptos(ctr_df)
+        ctr_df = fcrop.get_all_uptos(ctr_df)
     else:
-        ctr_df = fake_all_uptos(ctr_df)
+        ctr_df = fcrop.fake_all_uptos(ctr_df)
 
     # ==== ==== ==== h. before entry paths intersected by that poly
 
     # note: for artcc == real, this is the _one_ schedule before entry
     if ctr != 'ZZZ':
 
-        ctr_df = get_before_entry_single_artcc(ctr_df)
-
+        ctr_df = fcrop.get_before_entry_single_artcc(ctr_df,
+                                                     all_scheds_df, flown_pts_df)
     else:
 
-        ctr_df = make_bogus_before_entry(ctr_df)
+        ctr_df = fcrop.make_bogus_before_entry(ctr_df)
 
     # ==== k. final cleanup & prep for d.b. writes
 
